464c3f7a9fd236db48a984e0ab91531361368811
S4nfs <zyzzaestheticsgods@gmail.com>
2023-08-08 23:58:49 +0530

    getting accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -13,6 +13,9 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   output.push([dropPosition, bounciness, size, bucketLabel])
 }
 
+const numbers = [[10, .5, 16, 1], [200, .5, 16, 4], [350, .5, 16, 4], [600, .5, 16, 5]]
+
+
 function runAnalysis() {
 
   const bucket = _.chain(numbers)
@@ -30,10 +33,29 @@ function runAnalysis() {
   console.log("The Ball will probably fall into", bucket)
 }
 
-const numbers = [[10, .5, 16, 1], [200, .5, 16, 4], [350, .5, 16, 4], [600, .5, 16, 5]]
-
+function knn() {
+  return _.chain(numbers)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[2])
+    .last()
+    .first()
+    .parseInt()
+    .value()
+}
 
 
 function distance(point) {
   return Math.abs(point - predictionPoint)
+}
+
+//Getting Model Accuracy by comparing training set and test set
+function splitDataSet(data, testCount) {
+  const shuffled = _.shuffle(data)
+  const testSet = _.slice(shuffled, 0, testCount)   //upto testCount
+  const trainingSet = _.slice(shuffled, testCount)  //rest of data
+  return [testSet, trainingSet]
 }
