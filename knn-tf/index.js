82a879c105391b38866b230dcb0af99f8cffb373
S4nfs <zyzzaestheticsgods@gmail.com>
2023-08-18 23:05:58 +0530

    setup library

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -11,3 +11,16 @@ Distance =  √(lat - lot)2 + (long - long)2
 -Look at the 'k' top records
 -Average the label value of those top K records
 */
+
+require("@tensorflow/tfjs-node")
+const tf = require("@tensorflow/tfjs")
+const loadCSV = require('./load-csv')
+
+let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ['lat', 'long'],
+    labelColumns: ['price']
+})
+console.log(testFeatures)
+console.log(testLabels)
